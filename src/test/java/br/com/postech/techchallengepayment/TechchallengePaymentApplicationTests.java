df17d8cba6a323a34524dcf9979bfb7c46da5409
Alex Ferreira <alexjferreira94@yahoo.com.br>
2024-05-07 19:34:36 -0300

    initial commit

---
+++ src/test/java/br/com/postech/techchallengepayment/TechchallengePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallengepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallengePaymentApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
