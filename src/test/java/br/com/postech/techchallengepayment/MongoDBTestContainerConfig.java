7e4d14ce1589558c594d3c7ae6c79a93571dde98
Alex Ferreira <alexjferreira94@yahoo.com.br>
2024-05-18 20:03:00 -0300

    teste

--- src/test/java/br/com/postech/techchallengepayment/MongoDBTestContainerConfig.java
+++ src/test/java/br/com/postech/techchallengepayment/MongoDBTestContainerConfig.java
@@ -6,18 +6,20 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
 
 @Configuration
 @EnableMongoRepositories
 public class MongoDBTestContainerConfig {
-  @Container
-  public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest")
-      .withExposedPorts(27020);
 
-  static {
+  @Bean
+  public MongoDBContainer mongoDBContainer() {
+    MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest").withExposedPorts(27020);
     mongoDBContainer.start();
-    var mappedPort = mongoDBContainer.getMappedPort(27017);
-    System.setProperty("mongodb.container.port", String.valueOf(mappedPort));
+    return mongoDBContainer;
+  }
+
+  @Bean
+  public MongoTemplate mongoTemplate(MongoDBContainer mongoDBContainer) {
+    return new MongoTemplate(new SimpleMongoClientDatabaseFactory(mongoDBContainer.getReplicaSetUrl()));
   }
 }
