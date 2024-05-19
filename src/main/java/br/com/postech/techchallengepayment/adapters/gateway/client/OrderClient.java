f77421691e6a3ceebb1f3dc3b071684e5fa9c72b
Alex Ferreira <alexjferreira94@yahoo.com.br>
2024-05-19 17:18:00 -0300

    adding aws configurations

--- src/main/java/br/com/postech/techchallengepayment/adapters/gateway/client/OrderClient.java
+++ src/main/java/br/com/postech/techchallengepayment/adapters/gateway/client/OrderClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "order", url = "http://localhost:8080")
+@FeignClient(value = "order", url = "http://techchallenge-orders-env.eba-mpkpankc.us-east-2.elasticbeanstalk.com")
 public interface OrderClient {
 
   @PatchMapping(value = "/techchallenge/orders/{orderId}")
